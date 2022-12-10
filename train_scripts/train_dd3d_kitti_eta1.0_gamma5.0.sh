081a4815565ff4164d50fa06d88e46b23b4c9752
towardsautonomy <shubham@TowardsAutonomy.com>
2022-12-09 19:26:57 -0800

    add dequity specific configs and training scripts

--- train_scripts/train_dd3d_kitti_eta1.0_gamma5.0.sh
+++ train_scripts/train_dd3d_kitti_eta1.0_gamma5.0.sh
@@ -2,9 +2,9 @@
 
 WANDB_HOST="https://api.wandb.ai"
 WANDB_ENTITY="3d-object-detection"
-WANDB_API_KEY="be0ccba47f9974a1c8b64536844962f699c3aa65"
 export WANDB_ENTITY=$WANDB_ENTITY
-export WANDB_API_KEY=$WANDB_API_KEY
+# make sure to export WANDB_API_KEY in your environment
+# export WANDB_API_KEY=$WANDB_API_KEY
 
 wandb login --host $WANDB_HOST $WANDB_API_KEY
 ./scripts/train.py +experiments=dd3d_kitti_dla34_dequity \
